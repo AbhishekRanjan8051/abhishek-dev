d6fb7bfe0979c8d2466002014440caf7002104ea
Abhishek_Ranjan <abhishekranjan8051@gmail.com>
2021-08-13 19:50:03 +0530

    youtube search

---
+++ UNIT-3/coding/youtube/youtube-search/YT.js
@@ -0,0 +1,37 @@
+let videos_div = document.getElementById("videos");
+
+async function findVideos() {
+  videos_div.innerHTML = null;
+
+  let query = document.getElementById("query").value;
+  let res = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?q=${query}&key=AIzaSyD638E-zvBxypcCWCNIB7__e2en9CC2q8E&maxResults=20`
+  );
+
+  let data = await res.json();
+  console.log(data);
+
+  let { items } = data;
+
+  items = items.filter((el) => {
+    return el.id.videoId != undefined;
+  });
+
+  items.forEach(({ id: { videoId } }) => {
+    let div = document.createElement("div");
+
+    div.style.marginTop = "20px";
+
+    div.innerHTML = `<iframe
+      width="360"
+      height="315"
+           src="https://www.youtube.com/embed/${videoId}"
+      title="YouTube video player"
+      frameborder="0"
+      allowfullscreen
+      ></iframe> `;
+
+    videos_div.appendChild(div);
+  });
+}
+//   findVideos();
